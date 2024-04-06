5704b158ada3d933bd725102e755c54a5808475f
rishal-ERPGULF <rishal@htsqatar.com>
2024-04-06 10:20:52 +0530

    ADD -main text

--- src/pages/MainHome.tsx
+++ src/pages/MainHome.tsx
@@ -1,7 +1,9 @@
 const MainHome = () => {
   return (
     <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40">
-      main home
+      <div className="w-full flex justify-center h-24 items-center">
+        <h3 className="text-lg text-blue-600">Pricing</h3>
+      </div>
     </main>
   );
 };
